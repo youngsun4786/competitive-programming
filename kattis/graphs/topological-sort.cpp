3e1af984e8c661c405fe7b34eb1a5f3b3132f72e
Youngsun Yoo <youngsun123q@gmail.com>
2023-11-07 00:10:24 -0500

    topological-sort

--- kattis/graphs/topological-sort.cpp
+++ kattis/graphs/topological-sort.cpp
@@ -3,6 +3,8 @@
 #include<string>
 #include<map>
 #include<deque>
+#include<queue>
+#include<stack>
 #include<set>
 #include<algorithm>
 #include <utility>  
@@ -38,12 +40,57 @@ ll c[template_array_size];
 string s, t;
 ll ans = 0;
 
+vector<ll> edges[300005];
+ll indegree[300005];
+vector<ll> topsort;
+ll dp[100005];
 
 void solve(int tc = 0) {
-    // cin >> n >> m;
-    pll yo = mp(0, 1);
-    cout << yo.f << " " << yo.s << "\n";
+    cin >> n >> m >> s;
+
+    for (ll i = 0; i < m; ++i) {
+        cin >> x >> y;
+        x--; y--;
+        edges[x].push_back(y);
+        edges[y].push_back(x);
+
+        ++indegree[y];
+    }
+
+    queue<ll> q;
+    ll cnt = 0;
+    for (ll i = 0; i < n; ++i) {
+        if (indegree[i] == 0) q.push(i);
+    }
+    
+    while(!q.empty()) {
+        ++cnt;
+        ll x = q.front(); q.pop();
+        topsort.push_back(x);
+
+        for (ll next : edges[x]) {
+            --indegree[next];
+            if (indegree[next] == 0) q.push(next);
+        }
+    }
+
+    if (cnt < n) cout << -1 << "\n";
+
+    ll ans = 0;
+    for (char c = 'a'; c <= 'z'; c++) {
+        memset(dp, 0, sizeof(dp));
+
+        for (ll x : topsort) {
+            if (s[x] == c) ++ dp[x];
+            for (ll next : edges[x]) {
+                dp[next] = max(dp[next], dp[x]);
+            }
+            ans = max(ans, dp[x]);
+        }
+    }
 }
+
+// DAG - directed acyclic graph should not contain any CYCLES
 
 
 int main() {
